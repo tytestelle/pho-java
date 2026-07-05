afe25fd46490830f558a51093925da339911841f
tytestelle <ccszdk@gmail.com>
2026-07-05 21:07:57 +0800

    Update Application.java

--- src/main/ java/com/example/pho/Application.java
+++ src/main/ java/com/example/pho/Application.java
@@ -1,1 +1,14 @@
 
+package com.example.pho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling   // 开启定时任务支持
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
